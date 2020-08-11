1841c7e5598bea8b2d7476a18d3531236d75ebf1
root <root@ip-172-30-0-177.eu-west-1.compute.internal>
2020-08-11 23:05:03 +0000

    docker env

--- install.sh
+++ install.sh
@@ -126,12 +126,12 @@ sudo sed -i 's/6379/6370/g' incubator-openwhisk-devtools/docker-compose/docker-c
 sudo sed -i 's/6379/6370/g' incubator-openwhisk-devtools/docker-compose/docker-compose.packages.yml
 
 #Install OpenWhisk
-sudo nohup make -C incubator-openwhisk-devtools/docker-compose/ quick-start > install.log &
+sudo nohup make -C incubator-openwhisk-devtools/docker-compose/ quick-start > whisk_install.log &
 
 #Verify OpenWhisk installation
 
 if [ $? -eq 0 ]; then
-	echo -e "\nOpenWhisk components are being installed! Please check the progress in install.log file!\n"
+	echo -e "\nOpenWhisk components are being installed! Please check the progress in whisk_install.log file!\n"
 else 
 	echo ""
 	echo "OpenWhisk installation is not running! Please run this script again!"
